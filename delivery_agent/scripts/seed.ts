7b0e52abdacd208ca6c69fd86d703766babd2ccb
woaitsAryan <yoprince.ab@gmail.com>
2024-05-19 16:35:21 +0530

    feat: added seed scripts

---
+++ delivery_agent/scripts/seed.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import { faker } from '@faker-js/faker'
+import 'dotenv/config'
+import { DeliveryAgent } from '../src/models/delivery.model.js'
+
+async function seedData (): Promise<void> {
+  const DB_HOST = process.env.DB_HOST
+  const DB_PORT = process.env.DB_PORT
+  const DB_USER = process.env.DB_USER
+  const DB_PASSWORD = process.env.DB_PASSWORD
+  const DB_DATABASE = process.env.DB_DATABASE
+  if ((DB_HOST == null) || (DB_PORT == null) || (DB_USER == null) || (DB_PASSWORD == null) || (DB_DATABASE == null)) {
+    throw new Error('Missing environment variables')
+  }
+  const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`
+
+  await mongoose.connect(url)
+
+  for (let i = 0; i < 100; i++) {
+    const deliveryAgent = new DeliveryAgent({
+      _id: new mongoose.Types.ObjectId(),
+      name: faker.person.fullName(),
+      status: faker.helpers.arrayElement(['online', 'offline']),
+      phone: faker.phone.number()
+    })
+
+    await deliveryAgent.save()
+  }
+
+  await mongoose.connection.close()
+}
+
+seedData().catch(console.error)
---
+++ restaurant/scripts/seed.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import { faker } from '@faker-js/faker'
+import 'dotenv/config'
+import { randomInt } from 'crypto'
+import { Restaurant } from '../src/models/restaurant.model.js'
+
+async function seedData (): Promise<void> {
+  const DB_HOST = process.env.DB_HOST
+  const DB_PORT = process.env.DB_PORT
+  const DB_USER = process.env.DB_USER
+  const DB_PASSWORD = process.env.DB_PASSWORD
+  const DB_DATABASE = process.env.DB_DATABASE
+  if ((DB_HOST == null) || (DB_PORT == null) || (DB_USER == null) || (DB_PASSWORD == null) || (DB_DATABASE == null)) {
+    throw new Error('Missing environment variables')
+  }
+  const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`
+
+  await mongoose.connect(url)
+
+  for (let i = 0; i < 100; i++) {
+    const menuItems = randomInt(1, 10)
+    const menu = []
+    for (let i = 0; i < menuItems; i++) {
+      const item = faker.commerce.productName()
+      const price = randomInt(1, 100)
+      menu.push({ item, price })
+    }
+
+    const restro = new Restaurant({
+      _id: new mongoose.Types.ObjectId(),
+      name: faker.company.name(),
+      location: faker.location.streetAddress(),
+      menu,
+      status: faker.helpers.arrayElement(['online', 'offline']),
+      description: faker.lorem.sentence()
+    })
+
+    await restro.save()
+  }
+
+  await mongoose.connection.close()
+}
+
+seedData().catch(console.error)
